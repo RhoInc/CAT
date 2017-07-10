77f68d226de1c43be73d207aac9b85b004420e3e
Jeremy Wildfire <jwildfire@gmail.com>
2017-07-10 15:44:57 -0700

    Refactor CAT config workflow

--- build/cat.js
+++ build/cat.js
@@ -22,6 +22,114 @@
     cat.dataWrap = cat.wrap.append("div").attr("class", "cat-data footer");
   }
 
+  function initRendererSelect(cat) {
+    var rendererSection = cat.controls.wrap.append("div").attr("class", "control-section section1");
+
+    rendererSection.append("h3").text("1. Choose a Chart Type");
+    cat.controls.rendererSelect = rendererSection.append("select");
+    cat.controls.rendererSelect.selectAll("option").data(cat.config.renderers).enter().append("option").text(function (d) {
+      return d.name;
+    });
+
+    cat.controls.rendererSelect.on("change", function (d) {
+      cat.current = d3.select(this).select("option:checked").data()[0];
+      cat.controls.mainFunction.node().value = cat.current.main;
+      cat.controls.subFunction.node().value = cat.current.sub;
+      cat.controls.schema.node().value = cat.current.schema;
+
+      cat.controls.settingsType.filter(function (d) {
+        return this.value == "form";
+      }).property("disabled", cat.current.schema ? false : "disabled");
+    });
+  }
+
+  function initRendererConfig(cat) {
+    var versionSection = cat.controls.wrap.append("div").attr("class", "control-section section2");
+
+    versionSection.append("h3").text("2. Configure the Chart Type");
+    versionSection.append("span").text("Version: ");
+    cat.controls.versionSelect = versionSection.append("input");
+    cat.controls.versionSelect.node().value = "master";
+    versionSection.append("br");
+
+    versionSection.append("a").text("More Options").style("text-decoration", "underline").style("color", "blue").style("cursor", "pointer").on("click", function () {
+      d3.select(this).remove();
+      versionSection.selectAll("*").classed("hidden", false);
+    });
+
+    //specify the code to create the chart
+    versionSection.append("span").text(" Init: ").classed("hidden", true);
+    cat.controls.mainFunction = versionSection.append("input").classed("hidden", true);
+    cat.controls.mainFunction.node().value = cat.current.main;
+    versionSection.append("span").text(".").classed("hidden", true);
+    cat.controls.subFunction = versionSection.append("input").classed("hidden", true);
+    cat.controls.subFunction.node().value = cat.current.sub;
+    versionSection.append("br").classed("hidden", true);
+    //Webcharts versionSelect
+    versionSection.append("span").text("Webcharts Version: ").classed("hidden", true);
+    cat.controls.libraryVersion = versionSection.append("input").classed("hidden", true);
+    cat.controls.libraryVersion.node().value = "master";
+    versionSection.append("br").classed("hidden", true);
+
+    versionSection.append("span").text("Schema: ").classed("hidden", true);
+    cat.controls.schema = versionSection.append("input").classed("hidden", true);
+    cat.controls.schema.node().value = cat.current.schema;
+    versionSection.append("br").classed("hidden", true);
+  }
+
+  function initDataSelect(cat) {
+    var dataSection = cat.controls.wrap.append("div").attr("class", "control-section");
+    dataSection.append("h3").text("3. Choose a data Set");
+
+    cat.controls.dataFileSelect = dataSection.append("select");
+    cat.controls.dataFileSelect.selectAll("option").data(cat.config.dataFiles).enter().append("option").text(function (d) {
+      return d;
+    });
+  }
+
+  function initChartConfig(cat) {
+    var settingsSection = cat.controls.wrap.append("div").attr("class", "control-section");
+    settingsSection.append("h3").text("4. Customize the Chart");
+
+    settingsSection.append("span").text("Settings: ");
+
+    cat.controls.settingsStatus = settingsSection.append("div").style("font-size", "1.5em").style("float", "right").style("cursor", "pointer");
+
+    cat.settings.setStatus(cat, "no schema");
+
+    settingsSection.append("br");
+
+    settingsSection.append("input").attr("class", "radio").property("type", "radio").property("name", "settingsType").property("value", "text").property("checked", !cat.current.schema);
+    settingsSection.append("span").text("text");
+
+    settingsSection.append("input").attr("class", "radio").property("type", "radio").property("name", "settingsType").property("value", "form").property("checked", cat.current.schema).property("disabled", !cat.current.schema);
+    settingsSection.append("span").text("form");
+
+    cat.controls.settingsType = settingsSection.selectAll('input[type="radio"]');
+
+    cat.controls.settingsType.on("change", function (d) {
+      cat.settings.sync(cat);
+      if (this.value == "text") {
+        cat.controls.settingsInput.classed("hidden", false);
+        cat.controls.settingsForm.classed("hidden", true);
+      } else if (this.value == "form") {
+        cat.controls.settingsInput.classed("hidden", true);
+        cat.controls.settingsForm.classed("hidden", false);
+      }
+    });
+
+    settingsSection.append("br");
+
+    cat.controls.settingsInput = settingsSection.append("textarea").attr("rows", 10).style("width", "90%").text("{}").classed("hidden", cat.current.schema);
+
+    cat.controls.settingsForm = settingsSection.append("div").attr("class", "settingsForm").append("form").classed("hidden", !cat.current.schema);
+
+    if (cat.current.schema) {
+      cat.settings.setStatus(cat, "unknown");
+      cat.settings.makeForm(cat, null);
+    }
+  }
+
   function renderChart(cat) {
     var rendererObj = cat.controls.rendererSelect.selectAll("option:checked").data()[0];
     cat.chartWrap.selectAll("*").remove();
@@ -92,114 +200,42 @@
     tag.parentNode.insertBefore(script, tag);
   }
 
-  function init$1(cat) {
-    var settings = cat.config;
-    cat.current = settings.renderers[0];
-    var controlWrap = cat.controls.wrap;
+  function initSubmit(cat) {
+    var submitSection = cat.controls.wrap.append("div").attr("class", "control-section");
 
-    // minimize controls
-    var minimize = controlWrap.append("div").attr("class", "minimize").text("<<").style("float", "left").on("click", function () {
-      cat.controls.wrap.classed("hidden", true);
-      cat.wrap.insert("div", ":first-child").attr("class", "maximize").text(">>").on("click", function () {
-        cat.controls.wrap.classed("hidden", false);
-        d3.select(this).remove();
-      });
-    });
-
-    //submit
-    var submitSection = controlWrap.append("div").attr("class", "control-section");
     cat.controls.submitButton = submitSection.append("button").attr("class", "submit").text("Render Chart").on("click", function () {
       loadLibrary(cat);
     });
+  }
 
-    //Choose a renderer
-    var rendererSection = controlWrap.append("div").attr("class", "control-section");
-    rendererSection.append("span").text("Renderer: ");
-    cat.controls.rendererSelect = rendererSection.append("select");
-    cat.controls.rendererSelect.selectAll("option").data(settings.renderers).enter().append("option").text(function (d) {
-      return d.name;
-    });
+  function init$1(cat) {
+    cat.current = cat.config.renderers[0];
+    cat.controls.wrap.append("h2").text("Charting Application Tester 😼");
+    initSubmit(cat);
+    initRendererSelect(cat);
+    initRendererConfig(cat);
+    initDataSelect(cat);
+    initChartConfig(cat);
 
-    cat.controls.rendererSelect.on("change", function (d) {
-      cat.current = d3.select(this).select("option:checked").data()[0];
-      cat.controls.mainFunction.node().value = cat.current.main;
-      cat.controls.subFunction.node().value = cat.current.sub;
-      cat.controls.schema.node().value = cat.current.schema;
-
-      cat.controls.settingsType.filter(function (d) {
-        return this.value == "form";
-      }).property("disabled", cat.current.schema ? false : "disabled");
-    });
-
-    //Choose a version
-    var versionSection = controlWrap.append("div").attr("class", "control-section");
-    versionSection.append("span").text("  Version: ");
-    cat.controls.versionSelect = versionSection.append("input");
-    cat.controls.versionSelect.node().value = "master";
-
-    //specify the code to create the chart
-    var initSection = controlWrap.append("div").attr("class", "control-section");
-    initSection.append("span").text(" Init: ");
-    cat.controls.mainFunction = initSection.append("input");
-    cat.controls.mainFunction.node().value = cat.current.main;
-    initSection.append("span").text(".");
-    cat.controls.subFunction = initSection.append("input");
-    cat.controls.subFunction.node().value = cat.current.sub;
-
-    //Webcharts versionSelect
-    var versionSection = controlWrap.append("div").attr("class", "control-section");
-    versionSection.append("span").text("Webcharts Version: ");
-    cat.controls.libraryVersion = versionSection.append("input");
-    cat.controls.libraryVersion.node().value = "master";
-
-    //specify a schema
-    var schemaSection = controlWrap.append("div").attr("class", "control-section");
-
-    schemaSection.append("span").text("Schema: ");
-    cat.controls.schema = schemaSection.append("input");
-    cat.controls.schema.node().value = cat.current.schema;
-
-    //Edit the settings
-    var settingsSection = controlWrap.append("div").attr("class", "control-section");
-
-    settingsSection.append("span").text("Settings: ");
-    settingsSection.append("br");
-
-    settingsSection.append("input").attr("class", "radio").property("type", "radio").property("name", "settingsType").property("value", "text").property("checked", "checked");
-    settingsSection.append("span").text("text");
-
-    settingsSection.append("input").attr("class", "radio").property("type", "radio").property("name", "settingsType").property("value", "form").property("disabled", cat.current.schema ? null : "disabled");
-    settingsSection.append("span").text("form");
-
-    cat.controls.settingsType = settingsSection.selectAll('input[type="radio"]');
-
-    cat.controls.settingsType.on("change", function (d) {
-      if (this.value == "text") {
-        cat.controls.settingsInput.classed("hidden", false);
-        cat.controls.settingsForm.classed("hidden", true);
-      } else if (this.value == "form") {
-        cat.controls.settingsInput.classed("hidden", true);
-        cat.controls.settingsForm.classed("hidden", false);
-        cat.settings.makeForm(cat);
-      }
-    });
-
-    settingsSection.append("br");
-
-    cat.controls.settingsInput = settingsSection.append("textarea").attr("rows", 10).style("width", "90%").text("{}");
-
-    cat.controls.settingsForm = settingsSection.append("div").attr("class", "settingsForm").append("form").classed("hidden", true);
-
-    settingsSection.append("span").text("Settings Status: ");
-    cat.controls.settingsStatus = settingsSection.append("span").html("&#63;").style("color", "#888");
-
-    //Choose a data file
-    var dataSection = controlWrap.append("div").attr("class", "control-section");
-    dataSection.append("span").text("Data: ");
-    cat.controls.dataFileSelect = dataSection.append("select");
-    cat.controls.dataFileSelect.selectAll("option").data(settings.dataFiles).enter().append("option").text(function (d) {
-      return d;
-    });
+    // minimize controls
+    /*
+    cat.controls.minimize = controlWrap
+      .append("div")
+      .attr("class", "minimize")
+      .text("<<")
+      .style("float", "left")
+      .on("click", function() {
+        cat.controls.wrap.classed("hidden", true);
+        cat.wrap
+          .insert("div", ":first-child")
+          .attr("class", "maximize")
+          .text(">>")
+          .on("click", function() {
+            cat.controls.wrap.classed("hidden", false);
+            d3.select(this).remove();
+          });
+      });
+    */
   }
 
   var controls = {
@@ -271,28 +307,44 @@
     cat.config = defaultSettings; // just ignore the user settings for the moment. Will set up merge later.
   }
 
-  function makeForm(cat) {
+  function makeForm(cat, obj) {
     var version = cat.controls.versionSelect.node().value;
+
+    var formLayout = [{
+      type: "actions",
+      items: [{
+        type: "submit",
+        title: "Submit"
+      }]
+    }, "*"];
+
     var schemaPath = cat.config.rootURL + "/" + cat.current.name + "/" + version + "/" + cat.current.schema;
 
     var testPath = "../aeexplorer/settings-schema.json";
     //d3.json(schemaPath, function(error, schemaObj) {
+    d3.select(".settingsForm form").selectAll("*").remove();
     d3.json(testPath, function (error, schemaObj) {
-      $(".settingsForm form").jsonForm({
+      var myForm = $(".settingsForm form").jsonForm({
         schema: schemaObj,
+        value: obj,
+        form: formLayout,
         onSubmit: function onSubmit(errors, values) {
           if (errors) {
-            //console.log(errors);
+            cat.settings.setStatus(cat, "invalid");
+            cat.controls.settingsInput.node().value = JSON.stringify(cat.current.config);
           } else {
-            console.log(values);
+            cat.settings.setStatus(cat, "valid");
             cat.current.config = values;
-            loadLibrary(cat);
+            cat.controls.settingsInput.node().value = JSON.stringify(cat.current.config);
           }
         }
       });
+      //format the form a little bit so that we can dodge bootstrap
+      d3.select(".settingsForm form .form-actions input").text("Validate Settings").property("value", "Validate Settings");
       d3.selectAll("i.icon-plus-sign").append("span").text("+");
       d3.selectAll("i.icon-minus-sign").append("span").text("-");
-      console.log($(".settingsForm form").val());
+
+      console.log(myForm);
     });
   }
 
@@ -307,18 +359,51 @@
       return d3.select(this).property("checked");
     }).node().value;
 
-    console.log(settingType);
-
+    // set current config
     if (settingType == "text") {
-      console.log("text-y");
       cat.current.config = JSON.parse(cat.controls.settingsInput.node().value);
+      cat.settings.makeForm(cat, cat.current.config);
+    } else if (settingType == "form") {
+      //this submits the form (thus saving the most recent object)
+      $(".settingsForm form").trigger("submit");
     }
+  }
+
+  function setStatus(cat, statusVal) {
+    var statusOptions = [{
+      key: "valid",
+      symbol: "&#x2714;",
+      color: "green",
+      details: "Settings match the current schema. Click 'Render Chart' to draw the chart."
+    }, {
+      key: "invalid",
+      symbol: "&#x2718;",
+      color: "red",
+      details: "Settings do not match the current schema. You can still click 'Render Chart' to try to draw the chart, but it might not work as expected."
+    }, {
+      key: "unknown",
+      symbol: "?",
+      color: "blue",
+      details: "You've loaded a schema, but the setting have changed. Click 'Validate Settings' to see if they're valid or you can click 'Render Chart' and see what happens."
+    }, {
+      key: "no schema",
+      symbol: "NA",
+      color: "#999",
+      details: "No Schema loaded. Cannot validate the current settings. You can click 'Render Chart' and see what happens."
+    }];
+
+    var myStatus = statusOptions.filter(function (d) {
+      return d.key == statusVal;
+    })[0];
+
+    cat.controls.settingsStatus.html(myStatus.symbol).style("color", myStatus.color).attr("title", myStatus.details);
   }
 
   var settings = {
     makeForm: makeForm,
     reset: reset,
-    sync: sync
+    sync: sync,
+    setStatus: setStatus
   };
 
   function createCat() {
--- src/cat/controls/init.js
+++ src/cat/controls/init.js
@@ -1,12 +1,21 @@
-import { loadLibrary } from "../loadLibrary";
+import { initRendererSelect } from "./initRendererSelect";
+import { initRendererConfig } from "./initRendererConfig";
+import { initDataSelect } from "./initDataSelect";
+import { initChartConfig } from "./initChartConfig";
+import { initSubmit } from "./initSubmit";
 
 export function init(cat) {
-  var settings = cat.config;
-  cat.current = settings.renderers[0];
-  var controlWrap = cat.controls.wrap;
+  cat.current = cat.config.renderers[0];
+  cat.controls.wrap.append("h2").text("Charting Application Tester 😼");
+  initSubmit(cat);
+  initRendererSelect(cat);
+  initRendererConfig(cat);
+  initDataSelect(cat);
+  initChartConfig(cat);
 
   // minimize controls
-  var minimize = controlWrap
+  /*
+  cat.controls.minimize = controlWrap
     .append("div")
     .attr("class", "minimize")
     .text("<<")
@@ -22,150 +31,5 @@ export function init(cat) {
           d3.select(this).remove();
         });
     });
-
-  //submit
-  var submitSection = controlWrap
-    .append("div")
-    .attr("class", "control-section");
-  cat.controls.submitButton = submitSection
-    .append("button")
-    .attr("class", "submit")
-    .text("Render Chart")
-    .on("click", function() {
-      loadLibrary(cat);
-    });
-
-  //Choose a renderer
-  var rendererSection = controlWrap
-    .append("div")
-    .attr("class", "control-section");
-  rendererSection.append("span").text("Renderer: ");
-  cat.controls.rendererSelect = rendererSection.append("select");
-  cat.controls.rendererSelect
-    .selectAll("option")
-    .data(settings.renderers)
-    .enter()
-    .append("option")
-    .text(function(d) {
-      return d.name;
-    });
-
-  cat.controls.rendererSelect.on("change", function(d) {
-    cat.current = d3.select(this).select("option:checked").data()[0];
-    cat.controls.mainFunction.node().value = cat.current.main;
-    cat.controls.subFunction.node().value = cat.current.sub;
-    cat.controls.schema.node().value = cat.current.schema;
-
-    cat.controls.settingsType
-      .filter(function(d) {
-        return this.value == "form";
-      })
-      .property("disabled", cat.current.schema ? false : "disabled");
-  });
-
-  //Choose a version
-  var versionSection = controlWrap
-    .append("div")
-    .attr("class", "control-section");
-  versionSection.append("span").text("  Version: ");
-  cat.controls.versionSelect = versionSection.append("input");
-  cat.controls.versionSelect.node().value = "master";
-
-  //specify the code to create the chart
-  var initSection = controlWrap.append("div").attr("class", "control-section");
-  initSection.append("span").text(" Init: ");
-  cat.controls.mainFunction = initSection.append("input");
-  cat.controls.mainFunction.node().value = cat.current.main;
-  initSection.append("span").text(".");
-  cat.controls.subFunction = initSection.append("input");
-  cat.controls.subFunction.node().value = cat.current.sub;
-
-  //Webcharts versionSelect
-  var versionSection = controlWrap
-    .append("div")
-    .attr("class", "control-section");
-  versionSection.append("span").text("Webcharts Version: ");
-  cat.controls.libraryVersion = versionSection.append("input");
-  cat.controls.libraryVersion.node().value = "master";
-
-  //specify a schema
-  var schemaSection = controlWrap
-    .append("div")
-    .attr("class", "control-section");
-
-  schemaSection.append("span").text("Schema: ");
-  cat.controls.schema = schemaSection.append("input");
-  cat.controls.schema.node().value = cat.current.schema;
-
-  //Edit the settings
-  var settingsSection = controlWrap
-    .append("div")
-    .attr("class", "control-section");
-
-  settingsSection.append("span").text("Settings: ");
-  settingsSection.append("br");
-
-  settingsSection
-    .append("input")
-    .attr("class", "radio")
-    .property("type", "radio")
-    .property("name", "settingsType")
-    .property("value", "text")
-    .property("checked", "checked");
-  settingsSection.append("span").text("text");
-
-  settingsSection
-    .append("input")
-    .attr("class", "radio")
-    .property("type", "radio")
-    .property("name", "settingsType")
-    .property("value", "form")
-    .property("disabled", cat.current.schema ? null : "disabled");
-  settingsSection.append("span").text("form");
-
-  cat.controls.settingsType = settingsSection.selectAll('input[type="radio"]');
-
-  cat.controls.settingsType.on("change", function(d) {
-    if (this.value == "text") {
-      cat.controls.settingsInput.classed("hidden", false);
-      cat.controls.settingsForm.classed("hidden", true);
-    } else if (this.value == "form") {
-      cat.controls.settingsInput.classed("hidden", true);
-      cat.controls.settingsForm.classed("hidden", false);
-      cat.settings.makeForm(cat);
-    }
-  });
-
-  settingsSection.append("br");
-
-  cat.controls.settingsInput = settingsSection
-    .append("textarea")
-    .attr("rows", 10)
-    .style("width", "90%")
-    .text("{}");
-
-  cat.controls.settingsForm = settingsSection
-    .append("div")
-    .attr("class", "settingsForm")
-    .append("form")
-    .classed("hidden", true);
-
-  settingsSection.append("span").text("Settings Status: ");
-  cat.controls.settingsStatus = settingsSection
-    .append("span")
-    .html("&#63;")
-    .style("color", "#888");
-
-  //Choose a data file
-  var dataSection = controlWrap.append("div").attr("class", "control-section");
-  dataSection.append("span").text("Data: ");
-  cat.controls.dataFileSelect = dataSection.append("select");
-  cat.controls.dataFileSelect
-    .selectAll("option")
-    .data(settings.dataFiles)
-    .enter()
-    .append("option")
-    .text(function(d) {
-      return d;
-    });
+*/
 }
