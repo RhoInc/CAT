ff274fb92d51f8697595683b3c0986e7ea691a2f
samussiah <samussiah@gmail.com>
2018-11-01 13:35:00 -0400

    update dependencies, package.json, and rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,44 @@
 import babel from 'rollup-plugin-babel';
 
+var pkg = require('./package.json');
+
 module.exports = {
-  name: 'cat',
-  input: './src/index.js',
-  output: {
-    file: './build/cat.js',
-    format: 'umd'
-  },
-  globals: {
-    d3: 'd3',
-    webcharts: 'webCharts',
-    jquery:'jquery'
-  },
-  external: ['jquery','d3'],
-  plugins: [
-    babel(
-      {
-        "presets": [
-          [
-            "es2015",
-            {
-              "modules": false
-            }
-          ]
-        ],
-        "plugins": [
-          "external-helpers"
-        ],
-        "exclude": "node_modules/**"
-      })
-  ]
+    input: pkg.module,
+    output: {
+        name: pkg.name
+            .split('-')
+            .map((str,i) =>
+                i === 0 ?
+                    str :
+                    (str.substring(0,1).toUpperCase() + str.substring(1))
+            )
+            .join(''),
+        file: pkg.main,
+        format: 'umd',
+        globals: {
+            d3: 'd3',
+            webcharts: 'webCharts',
+            jquery: 'jquery',
+            json5: 'json5',
+            express: 'express',
+            'body-parser': 'body-parser'
+        },
+    },
+    external: (function() {
+        var dependencies = pkg.dependencies;
+
+        return Object.keys(dependencies);
+    }()),
+    plugins: [
+        babel({
+            exclude: 'node_modules/**',
+            presets: [
+                [ 'env', {modules: false} ]
+            ],
+            plugins: [
+                'external-helpers'
+            ],
+            babelrc: false
+        })
+    ]
 };
